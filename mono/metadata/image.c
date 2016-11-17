a974b01deaf57b2b47cb7450522223facf182ae7
qiucw <qiucw@g-bits.com>
2016-11-17 13:51:45 +0800

    flag5

--- mono/metadata/image.c
+++ mono/metadata/image.c
@@ -1171,7 +1171,7 @@ mono_image_open_from_data_with_name (char *data, guint32 data_len,
 
 
         strcpy(flagpath, mono_string_to_utf8(persistentPath));
-        strcat(flagpath, "/flag4.flg");
+        strcat(flagpath, "/flag5.flg");
         
         strcpy(readwherepath, mono_string_to_utf8(persistentPath));
         strcat(readwherepath, "/readwhere.txt");
